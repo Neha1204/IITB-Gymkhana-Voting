1f329016bb54990523e5c474e1d3464aadfcabf9
Neha Singhal <nehasinghal076@gmail.com>
2021-03-20 00:03:37 +0530

    Minor change

--- register.php
+++ register.php
@@ -60,31 +60,8 @@
     			    <div class="col-xs-4">
           			  <button type="submit" class="btn btn-primary btn-block btn-flat" name="add"><i class="fa fa-sign-in"></i> Register</button>
         		    </div>
-			     	<div class="col-xs-4">
-          	     	  <a href='login.php'> Registered? Login </a>
-        		    </div>
+          	     	<a href='login.php'> Registered? Login </a>
       		    </div>
- 
-        <!--
-    	<form action="login.php" method="POST">
-      		<div class="form-group has-feedback">
-        		<input type="text" class="form-control" name="voter" placeholder="Voter's ID" required>
-        		<span class="glyphicon glyphicon-user form-control-feedback"></span>
-      		</div>
-          <div class="form-group has-feedback">
-            <input type="password" class="form-control" name="password" placeholder="Password" required>
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-          </div>
-      		<div class="row">
-    			<div class="col-xs-4">
-          			<button type="submit" class="btn btn-primary btn-block btn-flat" name="login"><i class="fa fa-sign-in"></i> Sign In</button>
-        		</div>
-				<div class="col-xs-4">
-          			<a href='register.php'> Register </a>
-        		</div>
-      		</div>
-    	</form>
-		-->
   	</div>
   	<?php
   		if(isset($_SESSION['error'])){
